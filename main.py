69bac3b793c8334981eb8002db4624d69a7b1c3d
Eloy <eloy.campos.davellar@gmail.com>
2024-03-13 11:01:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 from bs4 import BeautifulSoup
 import requests
 
-html_text = requests.get('https://www.udemy.com/courses/search/?src=ukw&q=python').text
-soup = BeautifulSoup(html_text, 'lxml')
-courses = soup.find_all()
-print(courses)
+# URL de la página web
+url = 'https://www.encordador.es/'
+
+# Cordaje que quieres buscar
+cordaje_a_buscar = 'Babolat RPM Blast'
+
+# Realizar la solicitud HTTP
+response = requests.get(url)
+
+# Comprobar si la solicitud fue exitosa (código de estado 200)
+if response.status_code == 200:
+    # Obtener el contenido HTML de la página
+    html_text = response.text
+
+    # Crear un objeto BeautifulSoup para analizar el HTML
+    soup = BeautifulSoup(html_text, 'html.parser')
+
+    # Buscar el cordaje específico en el HTML
+    cordajes = soup.find_all('div', class_='b-img-t')
+
+    print(cordajes)
+        
+
+
+
